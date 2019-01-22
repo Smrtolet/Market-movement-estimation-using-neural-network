eb4ea44d59b95585156494b398769092a6795852
Marko Raguž <markoraguz07@gmail.com>
2019-01-23 00:35:08 +0100

    Updateana i fixana mreza

--- StockApiClient/src/LSTMNetwork.py
+++ StockApiClient/src/LSTMNetwork.py
@@ -10,12 +10,20 @@ end = datetime(2019,1,1)
 dataset_A = get_historical_data("AAPL", start, end, output_format='pandas')
 trainset = dataset_A.iloc[:,3:].values
 
+incrementSet = trainset.copy()[:,0]
+for i in range(0,60):
+    incrementSet[i]= 0
+for i in range(60,1006):
+    incrementSet[i] = trainset[i][0]-trainset[i-1][0]
+
+
 from sklearn.preprocessing import MinMaxScaler
 sc = MinMaxScaler(feature_range = (0, 1))
 bc = MinMaxScaler(feature_range = (0, 1))
+ic = MinMaxScaler(feature_range = (0, 1))
 training_set_scaled =sc.fit_transform(trainset)
 tmp = bc.fit_transform(trainset[:,0].reshape(-1,1))
-
+increment_scaled = ic.fit_transform(incrementSet.reshape(-1,1))
 
 X_train = []
 y_train = []
@@ -23,10 +31,10 @@ X_test = []
 y_test = []
 for i in range(60, 806):
     X_train.append(training_set_scaled[i-60:i])
-    y_train.append(training_set_scaled[i][0])
+    y_train.append(increment_scaled[i][0])
 for i in range(806, 1006):
     X_test.append(training_set_scaled[i-60:i])
-    y_test.append(training_set_scaled[i][0])
+    y_test.append(increment_scaled[i][0])
 X_train, y_train = np.array(X_train), np.array(y_train)
 X_test, y_test =  np.array(X_test), np.array(y_test)
 
@@ -70,17 +78,23 @@ regressor.add(Dense(units = 1))
 regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
 regressor.fit(X_train, y_train, epochs = 20, batch_size = 30)
 
-
-
 predicted_stock_price = regressor.predict(X_test)
-predicted_stock_price = bc.inverse_transform(predicted_stock_price)
+predicted_stock_price = ic.inverse_transform(predicted_stock_price)
+for i in range(0,len(predicted_stock_price)):
+    predicted_stock_price[i] = predicted_stock_price[i] + trainset[806+i][0]
+    
+real_stock_price = trainset.copy()[806:,0:1]
 
 
 # Visualising the results
-plt.plot(trainset[806:,0:1], color = 'red', label = 'Real AAPL Stock Price')
+plt.figure(figsize=(20,15))
+plt.plot(real_stock_price, color = 'red', label = 'Real AAPL Stock Price')
 plt.plot(predicted_stock_price, color = 'blue', label = 'Predicted AAPL Stock Price')
 plt.title('AAPL Stock Price Prediction')
 plt.xlabel('Days')
 plt.ylabel('AAPL Stock Price')
 plt.legend()
 plt.show()
+
+
+print("MSE is: ",sum((real_stock_price-predicted_stock_price)*(real_stock_price-predicted_stock_price))/len(real_stock_price))
